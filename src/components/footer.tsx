7eab2ddadf2c2412bdc1844bb77fee9a1a678aa8
dwk601 <10829539@uvu.edu>
2024-01-23 16:16:31 -0700

    Remove social media links from footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,22 +4,6 @@ export default function Footer() {
   return (
     <footer>
       <p>© {new Date().getFullYear()} Dongwook Kim. All rights reserved.</p>
-      <div>
-        <a
-          href="https://www.linkedin.com/in/dwk1/"
-          target="_blank"
-          rel="Dongwook Kim LinkedIn profile"
-        >
-          LinkedIn
-        </a>
-        <a
-          href="https://github.com/dwk601"
-          target="_blank"
-          rel="Dongwook Kim GitHub profile"
-        >
-          GitHub
-        </a>
-      </div>
     </footer>
   );
 }
