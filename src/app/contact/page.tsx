67b8c4c1fac0cb20eeab7ac0e2564e25fce408e4
dwk601 <10829539@uvu.edu>
2024-01-23 18:10:10 -0700

    Add @radix-ui/react-label component and update contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,64 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+
 export default function Contact() {
   return (
-    <div>
-      <main>
-        <h1>Contact Me</h1>
-        <p>...</p>
-      </main>
+    <div className="contact-container flex justify-center items-center">
+      <Card className="">
+        <CardHeader>
+          <CardTitle>Contact Information</CardTitle>
+          <CardDescription>
+            Reach out to me via these platforms.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label
+                  htmlFor="email"
+                  className="font-light text-muted-foreground"
+                >
+                  Email
+                </Label>
+                <a href="mailto:dongook.kim@outlook.com" id="email">
+                  dongook.kim@outlook.com
+                </a>
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label
+                  htmlFor="linkedin"
+                  className="font-light text-muted-foreground"
+                >
+                  LinkedIn
+                </Label>
+                <a href="https://www.linkedin.com/in/dwk1/" id="linkedin">
+                  dwk1
+                </a>
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label
+                  htmlFor="github"
+                  className="font-light text-muted-foreground"
+                >
+                  GitHub
+                </Label>
+                <a href="https://github.com/dwk601" id="github">
+                  dwk601
+                </a>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between"></CardFooter>
+      </Card>
     </div>
   );
 }
